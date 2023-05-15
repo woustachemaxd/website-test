f72a27b78176fb305bbaafb13823e8e0e2c5deef
woustachemaxd <deepanshu1115@gmail.com>
2023-05-15 22:39:45 +0530

    modified phone2

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   if (!isPC) {
     return (
       <div className="bg-black w-screen h-screen">
-        <p className="p-[70px] font-offBitDotNormal text-center text-white text-[14px]">
+        <p className="pt-[300px] font-offBitDotNormal text-center text-white text-[45px]">
           Please use a PC to view this content.
         </p>
       </div>
